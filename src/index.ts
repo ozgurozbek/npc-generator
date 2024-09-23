70f50315d70014f929a94444d0d7855101b2007d
Özgür Özbek <43455645+ozgurozbek@users.noreply.github.com>
2024-09-23 09:30:00 +0100

    Update version of npc-gen

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { generate, getNpcOptionsValues } from "./generate";
 export { debugNodeToString } from "./utils";
 
 //OZGUR 
-import {generate} from "./generate"
+// import {generate} from "./generate"
 // npx ts-node .\src\index.ts
 //@ts-ignore
-console.log(generate({npcOptions:{race:21}}))
+// console.log(generate({npcOptions:{race:21}}))
